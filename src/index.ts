7429977b4da8ff452ce555f22652bc05be7d853f
Flávio Félix <fau_felix30@hotmail.com>
2024-09-09 15:34:45 -0300

    feat: configurando tsc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { server } from './server/Server';
 
 server.listen(process.env.PORT || 3333, () => {
-  console.log(`App rodando na porta ${process.env.PORT} || 3333`);
+  console.log(`App rodando na porta ${process.env.PORT || 3333}`);
 });
